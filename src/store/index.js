7cf34510f8ee160a6ce530f2401c159ac01c0aec
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2021-01-13 13:14:30 +0100

    prevent duplicate brews

--- src/store/index.js
+++ src/store/index.js
@@ -25,18 +25,24 @@ const store = createStore({
         brewCoffee(state){
             const beans = state.selectedBeans;
             const recipe = state.selectedRecipe;
+            //Check if faulty coffee
             console.log(beans, recipe);
             if(!beans || !recipe){
                 alert("Something went wrong");
                 return;
             }
+
+            //Brew coffee
             const coffee=recipe;
             coffee.name=beans.name
             coffee.company=beans.company;
             coffee.id = coffee.name + coffee.company + coffee.type;
-            console.log(coffee);
-            state.latestBrews.unshift(coffee);
 
+            //remove any previous duplicate entries
+            state.latestBrews = state.latestBrews.filter(function(cof){console.log(cof.id!=coffee.id);return cof.id!=coffee.id});
+
+            //Add coffee to top of latest brews
+            state.latestBrews.unshift(coffee);
             this.commit("save")
         }
     },
